9821dfa0951f5039182c106a7a94386df564aba5
Irfan9716 <77392807+Irfan9716@users.noreply.github.com>
2022-10-28 23:35:53 +0530

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,16 @@
+function changeHomeImg() {
+    document.getElementById("mainImage").src =
+    "https://images.pexels.com/photos/1535162/pexels-photo-1535162.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+  }
+  function changeWorkImg() {
+    document.getElementById("mainImage").src =
+      "https://wallpaperaccess.com/full/268341.jpg";
+  }
+  function changeBlogImg() {
+    document.getElementById("mainImage").src =
+      "https://cdn.pixabay.com/photo/2016/06/09/08/49/blog-1445367_1280.jpg";
+  }
+  function changeAboutUsImg() {
+    document.getElementById("mainImage").src =
+      "https://media.istockphoto.com/photos/male-hand-writing-the-word-about-us-on-gray-background-promoting-the-picture-id1301177416?b=1&k=20&m=1301177416&s=170667a&w=0&h=cUCc8ST9CnzZi-1TTkwesHanWmeThlzgH6uNMnyZDgI=";
+  }
